9886d4147c1773d024668af069d6696acc09f763
Axel Mrak <axelmrak@gmail.com>
2024-07-14 02:15:51 -0300

    Add RootLayout component to layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function RootLayout({ children}) {
+  return (
+		<html>
+			<head />
+			<body>
+				{children}
+			</body>
+		</html>
+	);
+}
+
+export default RootLayout

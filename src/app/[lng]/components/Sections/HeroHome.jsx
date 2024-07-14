5f49f94ec7bc162ba4b458a1bbcb38234fb5e0e8
Axel Mrak <axelmrak@gmail.com>
2024-07-14 19:33:57 -0300

    Update HeroHome component layout and styling

--- src/app/[lng]/components/Sections/HeroHome.jsx
+++ src/app/[lng]/components/Sections/HeroHome.jsx
@@ -23,23 +23,23 @@ function HeroHome({ lng }) {
 	// }, [heroWords]);
 	// isFading ? 'opacity-0 word-fade-out' : 'opacity-100 word-fade-in'
 	return (
-		<section className="w-full h-[90vh] flex flex-row items-center justify-between">
-			<article className="w-1/2 font-light h-full flex flex-col items-start justify-center gap-6 pl-20">
+		<section className="w-full h-[90vh] flex flex-col gap-6 md:flex-row md:gap-12 items-center justify-between">
+			<article className="w-full md:w-1/2 font-light h-full flex flex-col items-start justify-center gap-6 pl-12 md:pl-20">
 				<span
-					className={`block transition-opacity duration-500 transform text-9xl font-light relative overflow-hidden 
+					className={`block transition-opacity duration-500 transform text-7xl md:text-8xl font-light relative overflow-hidden 
 					`}
 				>
 					Esnaj Software
 				</span>
 				<hr className="w-10/12 h-[3px] bg-accent" />
 				<div className="flex flex-col gap-4">
-					<h1 className="w-10/12 text-xl text-gray-dark font-light">
+					<h1 className="w-10/12 text-2xl md:text-xl text-gray-dark font-light">
 						{t('home.hero.description')}
 					</h1>
 				</div>
 			</article>
 			<article
-				className="w-3/4 h-full bg-left bg-contain bg-no-repeat"
+				className="w-full md:w-3/4 h-full bg-left bg-cover md:bg-contain bg-no-repeat"
 				style={{ backgroundImage: 'url(/images/hero-image.webp)' }}
 			/>
 		</section>
